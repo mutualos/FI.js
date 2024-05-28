2d51cbbaa4e860547444be512675f705e9048ceb
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-28 07:41:39 -0400

    Create pipes.js
    Copernicus.js variant

---
+++ organization/pipes.js
@@ -0,0 +1,17 @@
+const translations = {
+    loans: {  //match pipeID
+        "Principal": "principal",
+        "Rate_Over_Split": "annualRate",
+        "Maturity_Date": "maturityDate",
+        "Risk_Rating": "riskRating",
+        "Class_Code": "type",
+        "Portfolio": "ID",
+        "Previous_Average_Balance": "balance"
+        // Additional mappings as necessary
+    },
+    checking: {
+        "Portfolio": "ID",
+        "Previous_Average_Balance": "balance"
+    }
+};
+window.translations = translations; 
