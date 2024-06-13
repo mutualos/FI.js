145d1a972381bb3c4baadf8649dd802227e431ea
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-06-13 06:53:24 -0400

    Update pipes.js
    added openDate translation

--- organization/pipes.js
+++ organization/pipes.js
@@ -2,17 +2,18 @@ const translations = {
     loans: {  //match pipeID
         "Principal": "principal",
         "Rate_Over_Split": "annualRate",
+        "Date_Opened": "openDate",
         "Maturity_Date": "maturityDate",
         "Risk_Rating": "riskRating",
         "Class_Code": "type",
         "Portfolio": "ID",
-        "Previous_Average_Balance": "balance",
         "Branch_Number": "branch"
         // Additional mappings as necessary
     },
     checking: {
         "Portfolio": "ID",
         "Class_Code": "type",
+        "Date_Opened": "openDate",
         "Previous_Average_Balance": "balance",
         "PMTD_Service_Charge": "charges",
         "PMTD_Service_Charge_Waived": "chargesWaived",
